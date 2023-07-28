b4a4c565caedff431421fd6dbe899c8ccf4d121b
Robert Philipp <rob.philipp@gmail.com>
2023-07-28 08:00:06 -0400

    set password form

--- pages/passwords/token/[id].tsx
+++ pages/passwords/token/[id].tsx
@@ -2,8 +2,19 @@ import {useRouter} from "next/router";
 import Centered from "../../../components/Centered";
 import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
-import {Typography} from "@mui/material";
+import {
+    Button,
+    FormControl,
+    FormControlLabel,
+    FormGroup, IconButton,
+    Input, InputAdornment,
+    InputLabel, OutlinedInput,
+    TextField,
+    Typography
+} from "@mui/material";
 import {emptyUser} from "../../../components/users/RecipesUser";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import {useState} from "react";
 
 export default function PasswordByToken(): JSX.Element {
     const router = useRouter()
@@ -24,6 +35,14 @@ export default function PasswordByToken(): JSX.Element {
             })
     )
 
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
     if (isLoading) {
         return <Centered><Typography>Looking for Booboo&apos;s friends...</Typography></Centered>
     }
@@ -33,7 +52,66 @@ export default function PasswordByToken(): JSX.Element {
 
     return (
         <Centered>
-            <div>Hey {data?.data.name}, this is your token: {token ?? router.query.id}</div>
+            <FormGroup style={{maxWidth: 400}}>
+                <Typography style={{padding: 10}}>
+                    Hi {data?.data.name}. You&apos;ve been invited to join City Recipes.
+                    Please set your password.
+                </Typography>
+                <FormControl style={{margin: 10}}>
+                    <InputLabel htmlFor="enter-password">Enter Password</InputLabel>
+                    <OutlinedInput
+                        id="enter-password"
+                        aria-describedby="Enter Password"
+                        type={showPassword ? 'text' : 'password'}
+                        label="Enter Password"
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                <FormControl style={{margin: 10}}>
+                    <InputLabel htmlFor="confirm-password">Confirm Password</InputLabel>
+                    <OutlinedInput
+                        id="confirm-password"
+                        aria-describedby="Confirm Password"
+                        type={showPassword ? 'text' : 'password'}
+                        label="Confirm Password"
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                <FormControl style={{padding: 10}}>
+                    <Button
+                        variant="outlined"
+                        sx={{
+                            textTransform: 'none',
+                            width: 200,
+                            alignSelf: 'center',
+                        }}
+                    >
+                        Set Password
+                    </Button>
+                </FormControl>
+            </FormGroup>
         </Centered>
     )
 }
