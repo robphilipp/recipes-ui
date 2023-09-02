913242bebfe4f831e2a762bec63251099480a8e8
Robert Philipp <rob.philipp@gmail.com>
2023-09-02 08:19:34 -0400

    constraints refactoring; disable/enable user save

--- components/users/manage/AddUserForm.tsx
+++ components/users/manage/AddUserForm.tsx
@@ -12,7 +12,7 @@ import {
     Typography
 } from "@mui/material";
 import {Role, roleAtLeast, RoleType, roleTypeFrom} from "../Role";
-import React, {useReducer, useRef, useState} from "react";
+import React, {useReducer, useState} from "react";
 import {useRecipeSession} from "../../../lib/RecipeSessionProvider";
 import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
@@ -21,11 +21,15 @@ import Centered from "../../Centered";
 import SaveIcon from "@mui/icons-material/Save";
 import CancelIcon from "@mui/icons-material/Cancel";
 import useThrottling from "../../../lib/useThrottling";
+import {emailFormatConstraint, stringLengthConstraint} from "./constraints";
 
 const UserFormControl = styled(FormControl)(() => ({
     marginTop: 10,
 }))
 
+/**
+ * The user representation for this add-user form
+ */
 export type AddUserFormUser = {
     username: string
     email: string
@@ -33,14 +37,19 @@ export type AddUserFormUser = {
     roleDescription?: string
 }
 
-const INITIAL_USER = {username: "", email: "", role: RoleType.USER}
+// the initial user for this add-user form
+const INITIAL_USER: AddUserFormUser = {username: "", email: "", role: RoleType.USER}
 
+/**
+ * Reducer for the user accepts a current state, an action, and returns the updated state
+ * @param user The current user
+ * @param action The action to apply to the user (which, in this case, is the partial, updated user)
+ * @return The updated user
+ */
 const reducer = (user: AddUserFormUser, action: Partial<AddUserFormUser>): AddUserFormUser => ({...user, ...action})
-const stringLengthConstraint = (name: string, value: string, min: number = 6, max: number = 30): string =>
-    (value.length > 0 && (value.length < min || value.length > max)) ?
-        `length (${value.length}) of the ${name} must be between ${min} and ${max} characters` :
-        ""
-const EMAIL_REGEX: RegExp = /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)$/
+
+// used as the query-string for making REST call to determine the existence
+// of username and email
 export const NAME_EXISTENCE = "name_existence"
 export const EMAIL_EXISTENCE = "email_existence"
 
@@ -50,6 +59,12 @@ type Props = {
     onCancel: () => void
 }
 
+/**
+ * Form for adding a new user
+ * @param props The component properties
+ * @return A {@link JSX.Element}
+ * @constructor
+ */
 export default function AddUserForm(props: Props): JSX.Element {
     const {maxWidth = 600, onSave, onCancel} = props
 
@@ -68,32 +83,59 @@ export default function AddUserForm(props: Props): JSX.Element {
             })
     )
 
+    /*
+     * Holds the error state for username and email validity (empty error means there
+     * is no error)
+     */
     const [usernameError, setUsernameError] = useState("")
     const [emailError, setEmailError] = useState("")
 
+    /*
+     * Throttles the lookup of the username and email to ensure that the ones the user
+     * is attempting to add don't already exist in the system (these lookups sare REST calls)
+     */
     const usernameThrottle = useThrottling<string>(100, "", "username")
     const emailThrottle = useThrottling<string>(100, "", "email")
 
+    /**
+     * Throttled handler that checks the validity of the username and reports when
+     * the username already exists in the system, and updates the user (locally) with the
+     * new username.
+     * @param username The username to update
+     * @return An empty {@link Promise}
+     */
     async function handleUpdateUsername(username: string): Promise<void> {
         usernameThrottle(username, async name => {
             const response = await axios.get(`/api/users?${NAME_EXISTENCE}=${name}`)
             const error = stringLengthConstraint("username", name) +
                 (response.data.exists ? "Username already exists" : "")
             setUsernameError(error)
-            updateUser({username})
         })
+        updateUser({username})
     }
 
+    /**
+     * Throttled handler that checks the validity of the email and reports when
+     * the email already exists in the system, and updates the user (locally) with the
+     * new email.
+     * @param email The email to update
+     * @return An empty {@link Promise}
+     */
     async function handleUpdateEmail(email: string): Promise<void> {
         emailThrottle(email, async email => {
             const response = await axios.get(`/api/users?${EMAIL_EXISTENCE}=${email}`)
-            const error = stringLengthConstraint("email address", email) +
+            // const error = stringLengthConstraint("email address", email) +
+            const error = emailFormatConstraint(email) +
                 (response.data.exists ? "Email already exists" : "")
             setEmailError(error)
-            updateUser({email})
         })
+        updateUser({email})
     }
 
+    /**
+     * Handles updating the (local) user with the new role
+     * @param event The event holding the new role value
+     * @return An empty {@link Promise}     */
     function handleRoleSelected(event: SelectChangeEvent): void {
         roleTypeFrom(event.target.value)
             .onSuccess(selection => updateUser({role: selection}))
@@ -108,13 +150,26 @@ export default function AddUserForm(props: Props): JSX.Element {
 
     const roles: Array<Role> = data?.data || []
 
-
+    /**
+     * Enriches the user with the role description based on the user's role name
+     * @return An enriched (spiritually) user
+     */
     function enrichUser(): AddUserFormUser {
         const role = roles.find(role => role.name === user.role)
         if (role === undefined) {
             return user
         }
         return {...user, roleDescription: role.description}
+    }
+
+    /**
+     * @return `true` if the "save" button is disable; `false` otherwise
+     */
+    function saveDisabled(): boolean {
+        return usernameError.length > 0 ||
+            emailError.length > 0 ||
+            user.username.length === 0 ||
+            user.email.length === 0
     }
 
     return (<>
@@ -179,6 +234,7 @@ export default function AddUserForm(props: Props): JSX.Element {
                     size='small'
                     sx={{textTransform: 'none'}}
                     onClick={() => onSave(enrichUser())}
+                    disabled={saveDisabled()}
                 >
                     Save
                 </Button>
---
+++ components/users/manage/constraints.ts
@@ -0,0 +1,38 @@
+/*
+ * Constraint functions that returns an error message when the constraints are
+ * not met, and an empty string if the constraints are met
+ */
+
+/**
+ * String length constraint that returns an error message if the constraints are not met,
+ * and returns an empty string if the constraints are met
+ * @param name The field name associated with the value (used for the error message)
+ * @param value The value associated with the field-name (used to check)
+ * @param min The minimum length (inclusive)
+ * @param max The maximum length (inclusive)
+ * @return An error message if the length constraint is not met, otherwise an empty string
+ */
+export const stringLengthConstraint = (name: string, value: string, min: number = 6, max: number = 30): string =>
+    (value.length > 0 && (value.length < min || value.length > max)) ?
+        `length (${value.length}) of the ${name} must be between ${min} and ${max} characters` :
+        ""
+
+// default valid-email regex
+const EMAIL_REGEX: RegExp = /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)$/
+
+/**
+ * Email pattern constraint that returns an error message if the constraint is not met,
+ * and returns an empty string if the constraint is met
+ * @param email The email to test
+ * @param pattern The {@link RegExp} pattern
+ * @return an error message if the constraint is not met, and returns an empty string if
+ * the constraint is met
+ */
+export function emailFormatConstraint(email: string, pattern: RegExp = EMAIL_REGEX): string {
+    const lengthMatch = email.length >= 6 && email.length <= 50
+    const patternMatch = (email.match(pattern)?.length || -1) > 0
+    console.log(patternMatch)
+    return !lengthMatch || !patternMatch ?
+        `must be a valid email address between 6 and 50 characters` :
+        ""
+}
