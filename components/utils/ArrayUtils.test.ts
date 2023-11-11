8261984e43c524aa9a98ddaa3b587ee53cee3105
Robert Philipp <rob.philipp@gmail.com>
2023-11-11 17:39:06 -0500

    docs, zip

--- components/utils/ArrayUtils.test.ts
+++ components/utils/ArrayUtils.test.ts
@@ -1,21 +1,44 @@
 import {describe, expect, it} from "@jest/globals";
-import {duplicateCountsByKey, duplicatesByKey} from "./ArrayUtils";
+import {countByKey, duplicateCountsByKey, duplicatesByKey, zip} from "./ArrayUtils";
+
+const items = [
+    {key: 'a', value: 'A'},
+    {key: 'b', value: 'B'},
+    {key: 'c', value: 'C'},
+    {key: 'd', value: 'D'},
+    {key: 'a', value: 'A'},
+    {key: 'b', value: 'Aa'},
+    {key: 'b', value: 'Ab'},
+    {key: 'b', value: 'Ac'},
+    {key: 'b', value: 'Ad'},
+    {key: 'c', value: 'A'},
+    {key: 'c', value: 'Af'},
+    {key: 'e', value: 'E'},
+]
+
+describe('when grouping elements be a key using a key function', () => {
+    it('should be able to create the group-counts for each key', () => {
+        const expected = new Map([
+            ['a', 2],
+            ['b', 5],
+            ['c', 3],
+            ['d', 1],
+            ['e', 1]
+        ])
+        expect(countByKey(items, item => item.key)).toEqual(expected)
+    })
+    it('should be able to create groups for each key', () => {
+        const expected = new Map([
+            ['a', [{key: 'a', value: 'A'}, {key: 'a', value: 'A'}]],
+            ['b', [{key: 'b', value: 'B'}, {key: 'b', value: 'Aa'}, {key: 'b', value: 'Ab'}, {key: 'b', value: 'Ac'}, {key: 'b', value: 'Ad'}]],
+            ['c', [{key: 'c', value: 'C'}, {key: 'c', value: 'A'}, {key: 'c', value: 'Af'}]],
+            ['d', [{key: 'd', value: 'D'}]],
+            ['e', [{key: 'e', value: 'E'}]],
+        ])
+    })
+})
 
 describe('when determining duplicate elements in an array', () => {
-    const items = [
-        {key: 'a', value: 'A'},
-        {key: 'b', value: 'B'},
-        {key: 'c', value: 'C'},
-        {key: 'd', value: 'D'},
-        {key: 'a', value: 'A'},
-        {key: 'b', value: 'Aa'},
-        {key: 'b', value: 'Ab'},
-        {key: 'b', value: 'Ac'},
-        {key: 'b', value: 'Ad'},
-        {key: 'c', value: 'A'},
-        {key: 'c', value: 'Af'},
-        {key: 'e', value: 'E'},
-    ]
 
     it('should be able to calculate duplicate counts by key', () => {
         const expected = new Map([
@@ -33,5 +56,20 @@ describe('when determining duplicate elements in an array', () => {
             ['c', [{key: 'c', value: 'C'}, {key: 'c', value: 'A'}, {key: 'c', value: 'Af'}]]
         ])
         expect(duplicatesByKey(items, item => item.key)).toEqual(expected)
+    })
+})
+
+describe('when zipping together arrays', () => {
+    it('should return a zipped array with a length equal to the smallest size', () => {
+        const one = ['1', '2', '3', '4', '5']
+        const two = ['a', 'b', 'c', 'd']
+        const three = ['ay', 'bee', 'sea', 'dee', 'eeee']
+        const expected = [
+            ['1', 'a', 'ay'],
+            ['2', 'b', 'bee'],
+            ['3', 'c', 'sea'],
+            ['4', 'd', 'dee'],
+        ]
+        expect(zip(one, two, three)).toEqual(expected)
     })
 })
--- components/utils/ArrayUtils.ts
+++ components/utils/ArrayUtils.ts
@@ -1,3 +1,10 @@
+/**
+ * Creates a {@link Map} where the keys are the results of the specified {@link keyFn}, and
+ * the values are arrays holding the {@link items} that map to the specified {@link keyFn}
+ * @param items The items to group
+ * @param keyFn The function that calculates to the key on which to group
+ * @return A {@link Map} holding the keys and their associated groups
+ */
 export function groupByKey<T, K>(items: Array<T>, keyFn: (t1: T) => K): Map<K, Array<T>> {
     return items
         .map(item => [keyFn(item), item])
@@ -10,6 +17,13 @@ export function groupByKey<T, K>(items: Array<T>, keyFn: (t1: T) => K): Map<K, A
         )
 }
 
+/**
+ * Creates a {@link Map} the holds the number of times an item mapped to the same key
+ * (by the {@link keyFn}) occurs in the {@link items}.
+ * @param items The items to group
+ * @param keyFn The function that calculates to the key on which to group
+ * @return A {@link Map} holding the keys and their associated item count
+ */
 export function countByKey<T, K>(items: Array<T>, keyFn: (t1: T) => K): Map<K, number> {
     return new Map(Array
         .from(groupByKey(items, keyFn))
@@ -17,14 +31,55 @@ export function countByKey<T, K>(items: Array<T>, keyFn: (t1: T) => K): Map<K, n
     )
 }
 
+/**
+ * Creates a {@link Map} that holds the count for items that map to key. The {@link Map} holds
+ * the key (result of applying the {@link keyFn} to an item in the {@link items} list), to the
+ * number of times an item is mapped to that same key.
+ * @param items The items in which to look for duplicate mappings
+ * @param keyFn The function that maps an item to a key
+ * @return A {@link Map} holding the key (result of applying the {@link keyFn} to an item in
+ * the {@link items} list), to the number of times an item is mapped to that same key.
+ */
 export function duplicateCountsByKey<T, K>(items: Array<T>, keyFn: (t1: T) => K): Map<K, number> {
     return new Map<K, number>(
         Array.from(countByKey(items, keyFn)).filter(([_, count]) => count > 1)
     )
 }
 
+/**
+ * Creates a {@link Map} that holds keys onto which more than one item, in the {@link items} array
+ * get mapped by the {@link keyFn}. Each key is associated with an array of the items that are
+ * mapped to it, when at least 2 items are mapped to the key.
+ * @param items The items in which to look for duplicate mappings
+ * @param keyFn The function that maps an item to a key
+ * @return A {@link Map} holding the key (result of applying the {@link keyFn} to an item in
+ * the {@link items} list), to an array holding items that are to that same key, when at least
+ * 2 items are mapped to that key.
+ */
 export function duplicatesByKey<T, K>(items: Array<T>, keyFn: (t1: T) => K): Map<K, Array<T>> {
     return new Map<K, Array<T>>(
         Array.from(groupByKey(items, keyFn).entries()).filter(([_, elems]) => elems.length > 1)
     )
+}
+
+/**
+ * Zips together arrays with the resultant size being the size of the smallest array.
+ * Taken from [dominikandreas](https://gist.github.com/dominikandreas)' solution, from these
+ * [gists](https://gist.github.com/renaudtertrais/25fc5a2e64fe5d0e86894094c6989e10).
+ * @param arrays The arrays to zip together
+ * @return An array that has zipped together the arrays. The length of the array is the length
+ * of the smallest array in {@link arrays}
+ */
+export function zip<T>(...arrays: Array<Array<T>>): Array<Array<T>> {
+    // calculate the size of the smallest array
+    const minSize = Math.min(...arrays.map(array => array.length))
+
+    // grab the first array and then all the rest
+    const [head, ...tail] = arrays
+
+    return head
+        // limit the size of the head array to the smallest array
+        .slice(0, minSize)
+        // append each element of the tail to the corresponding element in the head
+        .map((elem, i) => [elem, ...tail.map(array => array[i])])
 }
