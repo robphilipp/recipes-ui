a8ec608bc7bba85e36f8fafdce2e0ab0518b790b
Robert Philipp <rob.philipp@gmail.com>
2024-06-30 15:18:42 -0400

    docs and commented out so that mongo cluster has replica set of size one

--- deployment/mongo/mongo_setup.sh
+++ deployment/mongo/mongo_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sleep 10
 mongosh --host mongo1:27017 <<EOF
   rs.initiate({
     "_id": "recipesReplicaSet",
@@ -9,20 +8,33 @@ mongosh --host mongo1:27017 <<EOF
         "_id": 0,
         "host": "mongo1:27017",
         "priority": 2
-      },
-      {
-        "_id": 1,
-        "host": "mongo2:27017",
-        "priority": 0
-      },
-      {
-        "_id": 2,
-        "host": "mongo3:27017",
-        "priority": 0
       }
-    ]
+     ]
   });
 EOF
+#mongosh --host mongo1:27017 <<EOF
+#  rs.initiate({
+#    "_id": "recipesReplicaSet",
+#    "version": 1,
+#    "members": [
+#      {
+#        "_id": 0,
+#        "host": "mongo1:27017",
+#        "priority": 2
+#      },
+#      {
+#        "_id": 1,
+#        "host": "mongo2:27017",
+#        "priority": 0
+#      },
+#      {
+#        "_id": 2,
+#        "host": "mongo3:27017",
+#        "priority": 0
+#      }
+#    ]
+#  });
+#EOF
 
 # ========================================================================
 # Update the code below for post-db-creation updates.
@@ -35,14 +47,22 @@ EOF
 #
 # this should allow the migration to continue from the imported changelog
 cd /deployment/deployment/mongo/backups; \
-mongoimport --db='recipeBook' --collection='recipes' --host='recipesReplicaSet/mongo1,mongo2,mongo3' --file='recipes-export.json' --bypassDocumentValidation; \
-mongoimport --db='recipeBook' --collection='changelog' --host='recipesReplicaSet/mongo1,mongo2,mongo3' --file='changelog-export.json' --maintainInsertionOrder; \
+mongoimport --db='recipeBook' --collection='recipes' --host='recipesReplicaSet/mongo1' --file='recipes-export.json' --bypassDocumentValidation; \
+mongoimport --db='recipeBook' --collection='changelog' --host='recipesReplicaSet/mongo1' --file='changelog-export.json' --maintainInsertionOrder; \
 cd /
+#cd /deployment/deployment/mongo/backups; \
+#mongoimport --db='recipeBook' --collection='recipes' --host='recipesReplicaSet/mongo1,mongo2,mongo3' --file='recipes-export.json' --bypassDocumentValidation; \
+#mongoimport --db='recipeBook' --collection='changelog' --host='recipesReplicaSet/mongo1,mongo2,mongo3' --file='changelog-export.json' --maintainInsertionOrder; \
+#cd /
 
 echo "checking changelog"
-mongosh --host 'recipesReplicaSet/mongo1,mongo2,mongo3' <<EOF
+mongosh --host 'recipesReplicaSet/mongo1' <<EOF
 use recipeBook;
 db.changelog.find();
 EOF
+#mongosh --host 'recipesReplicaSet/mongo1,mongo2,mongo3' <<EOF
+#use recipeBook;
+#db.changelog.find();
+#EOF
 
 
