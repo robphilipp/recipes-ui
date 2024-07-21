daaba56910f49697434e1402e515bb3938e5018f
Robert Philipp <rob.philipp@gmail.com>
2024-07-21 17:51:53 -0400

    explicity making generated directory

--- deployment/mongo/mongo_security_setup.sh
+++ deployment/mongo/mongo_security_setup.sh
@@ -4,5 +4,8 @@
 #  1. sets up the key-file for internal cluster communication
 #  2. sets up authentication for clients to access the database
 
+printf "(mongo_security_setup) creating internal replica key-file..."
+mkdir -p /deployment/deployment/mongo/generated
 openssl rand -base64 756 > /deployment/deployment/mongo/generated/internal-replica-key-file.key
 chmod 400 /deployment/deployment/mongo/generated/internal-replica-key-file.key
+printf "done\n"
